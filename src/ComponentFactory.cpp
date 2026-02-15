9bfaafc1522152ba722af21d63098bb8f5f9d8e3
YetAnotherMechanicusEnjoyer <ahmed.al-rawi@epitech.eu>
2026-02-15 10:48:39 +0100

    [ADD] src/ComponentFactory

---
+++ src/ComponentFactory.cpp
@@ -0,0 +1,31 @@
+#include "IComponent.hpp"
+#include "ComponentFactory.hpp"
+#include "Components/ClockComponent.hpp"
+#include "Components/InputComponent.hpp"
+#include "Components/OutputComponent.hpp"
+#include "Components/Gates.hpp"
+#include "Components/C4081.hpp"
+#include "Errors.hpp"
+#include <memory>
+
+namespace nts {
+ComponentFactory::ComponentFactory() {
+  _builders["input"] = []() { return std::make_unique<InputComponent>(); };
+  _builders["output"] = []() { return std::make_unique<OutputComponent>(); };
+  _builders["clock"] = []() { return std::make_unique<ClockComponent>(); };
+  _builders["true"] = []() { return std::make_unique<InputComponent>(); };
+  _builders["false"] = []() { return std::make_unique<InputComponent>(); };
+
+  _builders["and"] = []() { return std::make_unique<GateComponent>(Operators::ntsAnd); };
+  _builders["or"] = []() { return std::make_unique<GateComponent>(Operators::ntsOr); };
+  _builders["xor"] = []() { return std::make_unique<GateComponent>(Operators::ntsXor); };
+
+  _builders["4081"] = []() { return std::make_unique<C4081>(); };
+}
+
+std::unique_ptr<nts::IComponent> ComponentFactory::createComponent(const std::string &type) {
+  if (!_builders.contains(type))
+    throw NTSError("Unknown component type: " + type);
+  return _builders[type]();
+}
+}
