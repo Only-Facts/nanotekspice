e8a0c10386541d6912042e432262c580341c8ebc
YetAnotherMechanicusEnjoyer <ahmed.al-rawi@epitech.eu>
2026-02-15 10:50:34 +0100

    [ADD] include/Components/OutputComponent.hpp

---
+++ include/Components/OutputComponent.hpp
@@ -0,0 +1,23 @@
+/*
+** EPITECH PROJECT, 2026
+** OUTPUTCOMPONENT
+** File description:
+** nanotekspice
+*/
+
+#ifndef OUTPUTCOMPONENT
+  #define OUTPUTCOMPONENT
+  #include "IComponent.hpp"
+  #include "AComponent.hpp"
+
+namespace nts {
+class OutputComponent: public AComponent {
+public:
+  void simulate(std::size_t tick) override { (void)tick; }
+  nts::Tristate compute(std::size_t pin) override {
+    return getPinValue(1);
+  }
+};
+}
+
+#endif /* OUTPUTCOMPONENT */
