76521e46eff267f53b4b12615e40cbfeb0b797d5
raphael farge <raphael.farge@epitech.eu>
2026-03-08 15:59:43 +0000

    [FIX] undefined attributs

--- include/Components/C4514.hpp
+++ include/Components/C4514.hpp
@@ -1,67 +1,87 @@
-/*
-** EPITECH PROJECT, 2026
-** COMPONENT4514
-** File description:
-** nanotekspice
-*/
-
 #ifndef COMPONENT4514
   #define COMPONENT4514
+
   #include "AComponent.hpp"
   #include "IComponent.hpp"
 
 namespace nts {
+
 class C4514 : public AComponent {
 public:
   C4514() : AComponent("C4514") {}
   ~C4514() override = default;
 
-  void simulate(std::size_t tick) override {
+  void simulate(std::size_t tick) override
+  {
     if (tick <= _currentTick)
       return;
     _currentTick = tick;
-    const Tristate strobe = getPinValue(1);
-    if (strobe == True) {
-      const Tristate a = getPinValue(2);
-      const Tristate b = getPinValue(3);
-      const Tristate c = getPinValue(21);
-      const Tristate d = getPinValue(22);
-      if (a == Undefined || b == Undefined || c == Undefined || d == Undefined) {
-        _latched = -1;
-      } else {
-        _latched =
-          ((d == True) ? 8 : 0) |
-          ((c == True) ? 4 : 0) |
-          ((b == True) ? 2 : 0) |
-          ((a == True) ? 1 : 0);
-      }
-    } else if (strobe == Undefined) {
+
+    Tristate strobe = getPinValue(1);
+
+    if (_lastStrobe == True && strobe == False)
+      _latched = currentIndex();
+
+    if (strobe == Undefined)
       _latched = -1;
-    }
+
+    _lastStrobe = strobe;
   }
 
   Tristate runLogic(std::size_t pin) override {
     const int out = outIndexFromPin(pin);
-    if (out == -1)
+   if (out == -1)
       return Undefined;
-    const Tristate inhibit = getPinValue(23);
+
+    Tristate inhibit = getPinValue(23);
     if (inhibit == True)
       return False;
     if (inhibit == Undefined)
       return Undefined;
-    if (_latched < 0)
+
+    Tristate strobe = getPinValue(1);
+
+    int selected = -1;
+    if (strobe == True)
+      selected = currentIndex();
+    else if (strobe == False)
+      selected = _latched;
+    else
       return Undefined;
-    return (out == _latched) ? True : False;
+
+    if (selected < 0)
+      return Undefined;
+
+    return (out == selected) ? True : False;
   }
 
-  void setLink(std::size_t pin, IComponent &other, std::size_t otherPin) override {
+  void setLink(std::size_t pin, IComponent &other, std::size_t otherPin) override
+  {
     AComponent::setLink(pin, other, otherPin);
   }
 
 private:
   int _latched{-1};
+  Tristate _lastStrobe{Undefined};
 
-  static int outIndexFromPin(std::size_t pin) {
+  int currentIndex() const
+  {
+    Tristate a = getPinValue(2);
+    Tristate b = getPinValue(3);
+    Tristate c = getPinValue(21);
+    Tristate d = getPinValue(22);
+
+    if (a == Undefined || b == Undefined || c == Undefined || d == Undefined)
+      return -1;
+
+    return ((d == True) ? 8 : 0) |
+           ((c == True) ? 4 : 0) |
+           ((b == True) ? 2 : 0) |
+           ((a == True) ? 1 : 0);
+  }
+
+  static int outIndexFromPin(std::size_t pin)
+  {
     switch (pin) {
       case 11: return 0;
       case 9:  return 1;
@@ -83,5 +103,7 @@ private:
     }
   }
 };
+
 }
+
 #endif
